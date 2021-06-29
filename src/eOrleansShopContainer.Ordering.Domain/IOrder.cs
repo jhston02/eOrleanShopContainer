dffa3c74e37261b3f1d2ea9242bf65143ddad185
John H Stone <coleslaw.john@gmail.com>
2021-06-29 18:30:26 -0400

    Start order

---
+++ src/eOrleansShopContainer.Ordering.Domain/IOrder.cs
@@ -0,0 +1,30 @@
+﻿using Orleans;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eOrleansShopContainer.Ordering.Domain
+{
+    public interface IOrder : IGrainWithIntegerKey
+    {
+        void SetPaymentId(int id);
+
+        void SetBuyerId(int id);
+
+        void SetAwaitingValidationStatus();
+
+        void SetStockConfirmedStatus();
+
+        void SetPaidStatus();
+
+        void SetShippedStatus();
+
+        void SetCancelledStatus();
+
+        void SetCancelledStatusWhenStockIsRejected(IEnumerable<int> orderStockRejectedItems);
+
+        decimal GetTotal();
+    }
+}
